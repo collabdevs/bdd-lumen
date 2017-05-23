91461f0ca17168f444f26bd1e65f0cebf992000e
Daniel Medeiros <danielmmf@gmail.com>
2017-05-23 12:38:33 -0300

    passando os testes no angular

--- tests/acceptance/GroupCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-
-$I->amOnPage('/');
-//$I->fillField('username', 'davert');
-//$I->fillField('password', 'qwerty');
-//$I->click('LOGIN');
-$I->see('Lumen (5.4.6) (Laravel Components 5.4.*)');
